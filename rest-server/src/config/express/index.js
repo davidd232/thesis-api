04ab28e1d6e7c7482ba6f169e2160071b9939250
David Darley <daviddar232@gmail.com>
2018-01-31 11:35:24 -0800

    [init] - initialize the express class in config.

---
+++ rest-server/src/config/express/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import parser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import router from '../../routes/index';
+
+const middleWare = [
+  helmet(),
+  parser.json(),
+  parser.urlencoded({ extended: true }),
+  cors({
+    allowedHeaders: 'Content-Type, authorization',
+    methods: ['GET, POST, PUT, DELETE', 'OPTIONS'],
+  }),
+];
+
+class App {
+  constructor() {
+    this.express = express();
+    this.mountMiddleware();
+    this.mountRoutes();
+  }
+  mountMiddleware() {
+    this.express.use(...middleWare);
+  }
+  mountRoutes() {
+    this.express.use('/api', router);
+  }
+}
+
+export default new App();
