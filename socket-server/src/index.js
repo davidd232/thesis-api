a3057a473c399851f230ba98f48ea831aedaf8c8
David Darley <daviddar232@gmail.com>
2018-02-01 10:33:41 -0800

    [ops] - adding rooms to socket server

--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -2,14 +2,18 @@ const http = require('http');
 const SocketIo = require('socket.io');
 
 const log = require('./lib/log');
+const Rooms = require('./rooms');
 
 const server = http.createServer();
 const io = SocketIo(server);
+const rooms = new Rooms(io);
 
 io.on('connection', (client) => {
   client.removeAllListeners();
   log('client connected');
-  client.join('yooo');
+  const { roomId } = client.handshake.query;
+  const room = rooms.findOrCreate(roomId || 'default');
+  client.join(room.get('id'));
 })
 
 const PORT = process.env.PORT || 4155;
